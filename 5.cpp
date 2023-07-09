1880ef9e147469a7ae68505e45e2f75418e0fd39
Krish Vijay Ramani <136609165+KrishRamani2@users.noreply.github.com>
2023-07-09 19:23:38 +0530

    Add files via upload

---
+++ 5.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   float n,s;
+   cout<<"Enter n";
+   cin>>n;
+   s = (n*(n+1))/2;
+   cout<<"Sum of n natural numbers is "<<s;
+   return 0 ; 
+}
